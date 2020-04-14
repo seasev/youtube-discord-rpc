e10bf916ef7f00cfe8b0f2710db5c846f77130c2
Sean W <ssw663@gmail.com>
2020-04-14 18:59:07 -0400

    Connect to Discord and control with tray icon

---
+++ YouTubeDiscordRPC/DiscordClient.cs
@@ -0,0 +1,100 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.Text;
+
+namespace YouTubeDiscordRPC
+{
+    static class DiscordClient
+    {
+        // Important variables and constants
+        private const string clientID = "699671462117638166";
+
+        private static LogLevel logLevel = LogLevel.Info;
+
+        private static int discordPipe = -1;
+
+        private static DiscordRpcClient client;
+
+        public static bool IsRunning { get; private set; } = false;
+
+        private static StringBuilder word = new StringBuilder();
+
+
+        // Main program
+        public static void Initialize(int pipe = -1)
+        {
+            discordPipe = pipe;
+
+            MainPresence();
+        }
+
+
+        // Presence example
+        private static void MainPresence()
+        {
+            client = new DiscordRpcClient(clientID, pipe: discordPipe)
+            {
+                Logger = new DiscordRPC.Logging.ConsoleLogger(logLevel, true)
+            };
+
+            client.OnReady += (sender, msg) =>
+            {
+                Console.WriteLine("Connected to discord with user {0}", msg.User.Username);
+            };
+
+            client.OnPresenceUpdate += (sender, msg) =>
+            {
+                Console.WriteLine("Presence updated");
+            };
+
+            client.Initialize();
+
+            UpdatePresence();
+
+            IsRunning = true;
+        }
+
+        // Update the presence in Discord
+        private static void UpdatePresence()
+        {
+            client.SetPresence(new RichPresence()
+            {
+                Details = "A Basic Presence",
+                State = "In-Game",
+                Timestamps = Timestamps.FromTimeSpan(20)
+            });
+        }
+
+        // Stop showing the rich presence in Discord
+        public static void Stop()
+        {
+            client.ClearPresence();
+
+            IsRunning = false;
+        }
+
+        // Re-enable the rich presence
+        public static void Start()
+        {
+            UpdatePresence();
+
+            IsRunning = true;
+        }
+
+        // Toggle the state of the rich presence
+        public static void Toggle()
+        {
+            if (IsRunning)
+                Stop();
+            else
+                Start();
+        }
+
+        // Destroy the client
+        public static void Exit()
+        {
+            client.Dispose();
+        }
+    }
+}
--- YouTubeDiscordRPC/MainApplicationContext.cs
+++ YouTubeDiscordRPC/MainApplicationContext.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace YouTubeDiscordRPC
@@ -11,24 +7,75 @@ namespace YouTubeDiscordRPC
     {
         private NotifyIcon trayIcon;
 
+        private MenuItem stateItem;
+        private MenuItem exitItem;
+
         public MainApplicationContext()
         {
+            stateItem = new MenuItem("Stop", Stop);
+            exitItem = new MenuItem("Exit", Exit);
+
             this.trayIcon = new NotifyIcon()
             {
+                Text = "YouTube Discord RPC Client",
                 Icon = Properties.Resources.AppIcon,
                 ContextMenu = new ContextMenu(new MenuItem[]
                 {
+                    new MenuItem("Stop", Stop),
                     new MenuItem("Exit", Exit)
                 }),
                 Visible = true
             };
+
+            this.trayIcon.Click += NotifyIcon_Click;
         }
 
+        // Change the context menu items based on RPC state
+        void UpdateContextMenu()
+        {
+            trayIcon.ContextMenu.Dispose();
+            trayIcon.ContextMenu = new ContextMenu(new MenuItem[]
+            {
+                DiscordClient.IsRunning ? new MenuItem("Stop", Stop) : new MenuItem("Start", Start),
+                new MenuItem("Exit", Exit)
+            });
+        }
+
+        // Pause the RPC
+        void Stop(object sender, EventArgs e)
+        {
+            DiscordClient.Stop();
+            UpdateContextMenu();
+        }
+
+        // Resume the RPC
+        void Start(object sender, EventArgs e)
+        {
+            DiscordClient.Start();
+            UpdateContextMenu();
+        }
+
+        // Exit the application
         void Exit(object sender, EventArgs e)
         {
-            this.trayIcon.Visible = false;
+            DiscordClient.Exit();
 
+            this.trayIcon.Visible = false;
             Application.Exit();
+        }
+
+        // Runs when the tray icon is clicked
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            var eventArgs = e as MouseEventArgs;
+
+            switch (eventArgs.Button)
+            {
+                case MouseButtons.Left:
+                    DiscordClient.Toggle();
+                    UpdateContextMenu();
+                    break;
+            }
         }
     }
 }
