fdab7ca34170ae6e6504185622552706961aa787
Sean W <ssw663@gmail.com>
2020-04-14 12:22:53 -0400

    Create system tray app context

---
+++ YouTubeDiscordRPC/MainApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YouTubeDiscordRPC
+{
+    class MainApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public MainApplicationContext()
+        {
+            this.trayIcon = new NotifyIcon()
+            {
+                Icon = Properties.Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[]
+                {
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            this.trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
